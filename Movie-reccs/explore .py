645659776b9ef2d89f9ba67cc9942295b2a14df9
Swarit-dev-st <swaritschauhan@gmail.com>
2026-06-15 16:36:16 +0530

    analysisof user activity and rating distribution

--- Movie-reccs/explore .py
+++ Movie-reccs/explore .py
@@ -21,3 +21,18 @@ all_ratings = [r[2] for r in ratings]
 print(f"Highest Rating: {max(all_ratings)}")
 print(f"Lowest Rating: {min(all_ratings)}")
 print(f"Average Rating: {sum(all_ratings) / len(all_ratings):.2f}")
+
+user_counts = {}
+for user_id, movie_id, rating in ratings:
+    if user_id not in user_counts:
+        user_counts[user_id] = 0
+    user_counts[user_id] += 1
+
+sorted_user = sorted(user_counts.items(), key=lambda x: x[1], reverse=True)
+
+print("\nTop 5 users by number of ratings:")
+for user_id,count in sorted_user[:5]:
+    print(f"User {user_id} rated {count} movies")
+
+avg_ratings_per_user = len(ratings) / len(user_counts)
+print (f"\nAverage movies Rated per User: {avg_ratings_per_user:.2f}")
